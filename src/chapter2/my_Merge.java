43123f8a3f5e5fe73bc8791a9a708efae108e2b7
slevin1994 <slevin1994@163.com>
2017-09-15 09:26:44 +0800

    归并排序again

--- src/chapter2/my_Merge.java
+++ src/chapter2/my_Merge.java
@@ -61,11 +61,27 @@ public class my_Merge {
 //        
 //    }
     //book
+//    public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+//    	int i=lo,j=mid+1;
+//    	for(int k=lo;k<=hi;k++)
+//    		aux[k]=a[k];
+//    	for(int k=lo;k<=hi;k++) {
+//    		if(i>mid)
+//    			a[k]=aux[j++];
+//    		else if(j>hi)
+//    			a[k]=aux[i++];
+//    		else if(less(aux[j],aux[i]))
+//    			a[k]=aux[j++];
+//    		else
+//    			a[k]=aux[i++];
+//    	}
+//    }
+    //again
     public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
-    	int i=lo,j=mid+1;
+    	int i=lo, j=mid+1;
     	for(int k=lo;k<=hi;k++)
     		aux[k]=a[k];
-    	for(int k=lo;k<=hi;k++) {
+    	for (int k=lo;k<=hi;k++) {
     		if(i>mid)
     			a[k]=aux[j++];
     		else if(j>hi)
@@ -75,6 +91,7 @@ public class my_Merge {
     		else
     			a[k]=aux[i++];
     	}
+    	
     }
     
 
